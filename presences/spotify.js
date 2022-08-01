a4b92de4b8061c9ae5848395af0ea8c41753663b
Milk <95538243+MilkGatas@users.noreply.github.com>
2022-08-02 02:23:28 +0800

    Create spotify.js

---
+++ presences/spotify.js
@@ -0,0 +1,26 @@
+const rpcGenerator = require('discordrpcgenerator');
+const client = require('..');
+const config = require('.././config.json');
+const chalk = require('chalk');
+if (config.mode === 'spotify') {
+    try {
+        client.on('ready', () => {
+            const presence = rpcGenerator.createSpotifyRpc(client)
+                .setAssetsLargeImage(config.settings.spotify.largeImageKey)
+                .setAssetsSmallImage(config.settings.spotify.smallImageKey)
+                .setDetails(config.settings.spotify.name)
+                .setState(config.settings.spotify.details)
+                .setStartTimestamp(config.settings.spotify.startTimestamp || Date.now())
+                .setEndTimestamp(config.settings.spotify.endTimestamp || null);
+
+            client.user.setPresence(presence.toDiscord());
+          
+            console.log(chalk.hex('#800080')('RPC enabled successfully!'));
+            console.log(chalk.hex('#800080')('Spotify: ' + config.settings.spotify.name));
+            console.log(chalk.hex('#800080')('Status: ' + config.status));
+
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
